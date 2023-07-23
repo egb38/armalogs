5397ca3d7e54ae670db1b5f0ce8a529357be31df
Etienne <139290580+egb38@users.noreply.github.com>
2023-07-23 10:19:11 -0700

    license notices

--- i18n.js
+++ i18n.js
@@ -1,4 +1,23 @@
-// originally copied from https://phrase.com/blog/posts/step-step-guide-javascript-localization/
+/*
+# Licensed to the egb38 under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  egb38 licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   https://github.com/egb38/armalogs/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+*/
+
+// inspired from https://phrase.com/blog/posts/step-step-guide-javascript-localization/
 
 // defaults to French
 var currentLocale = "fr";
